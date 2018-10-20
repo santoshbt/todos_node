b097527edc0bfa72e75b6bc9724cc1cb8c14b78f
santoshbt <bt.santosh01@gmail.com>
2018-10-20 17:18:29 -0300

    fetched the single todo y id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
 var app = express();
 
 app.use(bodyParser.json());
@@ -27,6 +28,20 @@ app.get('/todos', (req, res) => {
   }, (e) => {
       res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    res.status(404).send('Id not valid');
+  }else{
+    Todo.findById(id).then((todo) => {
+      if(!todo) {
+        res.send('Todo not found');
+      }
+      res.send({todo});
+    }).catch((e) => res.status(400).send());
+  }
 });
 
 
